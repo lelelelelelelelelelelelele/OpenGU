e1aba8a707ef5bfa6d9507c4f365319fb39e6e43
Frank <frank04180@outlook.com>
2024-12-04 20:48:44 +0800

    readthedocs

---
+++ docs/conf.py
@@ -0,0 +1,31 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'OpenGU'
+copyright = '2024, Bowen Fan,Zhilin Guo'
+author = 'Bowen Fan,Zhilin Guo'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['myst_parser']
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_title = "OpenGU Documentation"  # HTML页面标题
+# html_logo = "path/to/logo.png"  # logo
+# html_favicon = "path/to/favicon.ico"  # favicon
