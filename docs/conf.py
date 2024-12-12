2fc84c8423b33317d7b9451cca3b97c918d2688f
艾煜明 <1246132896@qq.com>
2024-12-12 13:30:40 +0800

    framework

--- docs/conf.py
+++ docs/conf.py
@@ -5,24 +5,30 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
 
 project = 'OpenGU'
-copyright = '2024, Bowen Fan, Yuming Ai, Zhilin Guo'
-author = 'Bowen Fan, Yuming Ai, Zhilin Guo'
-release = '1.0'
+copyright = '2024, Bowen Fan,Zhilin Guo'
+author = 'Bowen Fan,Zhilin Guo'
+release = '0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
-
+extensions = ['myst_parser']
+source_dir = ['.']
 templates_path = ['_templates']
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-language = 'python'
+
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+html_title = "OpenGU Documentation"  # HTML页面标题
+# html_logo = "path/to/logo.png"  # logo
+# html_favicon = "path/to/favicon.ico"  # favicon
